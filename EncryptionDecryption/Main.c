81f3311c36011643b2790255ad65fdc41197aee9
JaeHyeon_Lee <project0858@gmail.com>
2025-08-20 16:25:08 +0900

    feat: 파일 암호화
    시크릿 키로 문자 하나씩 변환하여 `*.enc` 형식의 파일 생성

--- EncryptionDecryption/Main.c
+++ EncryptionDecryption/Main.c
@@ -14,13 +14,17 @@ typedef struct
 } Context;
 
 /* --- Inline XOR 암호화 1 --- */
-__inline void xorEncryptSimple(char* data, size_t len, const char* key) {
+void XorEncryptSimple(char* data, size_t len, const char* key)
+{
 	size_t keyLen = strlen(key);
 	for (size_t i = 0; i < len; i++)
+	{
+		printf("%c",data[i] ^ key[i % keyLen] ^ key[i % keyLen]);
 		data[i] ^= key[i % keyLen];
+	}
 }
 /* --- 파일 읽기 ---*/
-int readFile(const char* filename, char* buffer, size_t maxLen, size_t* outLen)
+int ReadFile(const char* filename, char* buffer, size_t maxLen, size_t* outLen)
 {
 	FILE* fp = fopen(filename, "rb");
 	if (!fp) return 0;
@@ -30,35 +34,44 @@ int readFile(const char* filename, char* buffer, size_t maxLen, size_t* outLen)
 	*outLen = len;
 	return 1;
 }
+int WriteFile(const char* filename, const char* buffer, size_t len)
+{
+	FILE* fp = fopen(filename, "wb");
+	if (!fp) return 0;
+	fwrite(buffer, 1, len, fp);
+	fclose(fp);
+	return 1;
+}
 /* --- 콘솔 메뉴 출력 --- */
-void printMenu(void)
+void PrintMenu(void)
 {
 	printf("=== 파일 암호화 & 전자서명 도구 ===\n");
 	printf("1. 평문 파일 읽기\n");
+	printf("2. 파일 암호화\n");
 	printf("6. 종료\n");
 	printf("원하는 메뉴 번호 입력: ");
 }
 
 int main(void)
 {
-	Context ctx = { "", "secretkey", xorEncryptSimple };
+	Context ctx = { "", "secretkey", XorEncryptSimple };
 	char buffer[MAX_BUFF_SIZE]; // 입출력에 사용될 버퍼
 	size_t dataLen = 0; // 파일 크기
 	unsigned char signature = 0;
 	int runnging = 1;
 
 	while (runnging) {
-		printMenu(); // 메뉴 출력
+		PrintMenu(); // 메뉴 출력
 		int choice; // 메뉴 번호 입력받을 변수
 		scanf_s("%d%*c", &choice); // 입력 받기, 변수의 메모리 주소를 인자로, %*c 줄바꿈 버림
 
 		switch (choice)
 		{
-		case 1: // read plain text
+		case 1: // 파일읽기
 			printf("읽을 파일명 입력: ");
 			fgets(ctx.filename, sizeof(ctx.filename), stdin);	// 구조체의 filename에 입력값 저장
 			ctx.filename[strcspn(ctx.filename, "\n")] = '\0';	// 구조체에 저장된 filename 입력시 저장된 개행 문자 제거
-			if (readFile(ctx.filename, buffer, MAX_BUFF_SIZE, &dataLen))	// filename, 입출력 버퍼, 버퍼크기, 파일크기 변수의 주소
+			if (ReadFile(ctx.filename, buffer, MAX_BUFF_SIZE, &dataLen))	// filename, 입출력 버퍼, 버퍼크기, 파일크기 변수의 주소
 			{
 				printf("파일 내용 (%zu 바이트):\n", dataLen);	// 파일 크기 확인
 				fwrite(buffer, 1, dataLen, stdout);				// 파일 내용 출력, 버퍼 사이즈 만큼씩, stdout을 파일이름으로 변경시 파일에 작성
@@ -67,6 +80,39 @@ int main(void)
 			else
 				printf("파일을 읽을 수 없습니다.\n");
 			break;
+
+		case 2: // 암호화 + 파일 쓰기
+			printf("암호화할 파일명 입력: ");
+			fgets(ctx.filename, sizeof(ctx.filename), stdin);	// 파일명 저장
+			ctx.filename[strcspn(ctx.filename, "\n")] = '\0';	// 입력값의 개행문자 제거
+
+			if (ReadFile(ctx.filename, buffer, MAX_BUFF_SIZE, &dataLen))
+			{
+				ctx.cryptFunc(buffer, dataLen, ctx.key);
+
+				char outFile[300];
+				snprintf(outFile, sizeof(outFile), "%s.enc", ctx.filename);
+
+				if (WriteFile(outFile, buffer, dataLen))
+				{
+					printf("파일 저장 완료\n");
+				}
+				else
+				{
+					printf("파일 저장 실패\n");
+				}
+			}
+			else
+			{
+				printf("파일 읽기 실패\n");
+			}
+			break;
+
+
+
+
+
+
 
 
 		case 6:
